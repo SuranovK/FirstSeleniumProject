a55365dbd8d83ff5704512efaa89a2c4c7609866
Khasanboi <khsuranov@gmail.com>
2018-06-27 14:42:44 -0500

    Not added yet

--- src/com/cbt/SeleniumDemo.java
+++ src/com/cbt/SeleniumDemo.java
@@ -1,27 +1,17 @@
 package com.cbt;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;;
 
 public class SeleniumDemo {
+
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver",
-				"/Users/nasyikatiusupova/Documents/selenium dependencies/drivers/chromedriver");
+		System.setProperty("WebDriver.chrome.driver", "/Users/asanbaisuranov/Documents/selenium dependencies/drivers/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://google.com");
-		String expected = "Google";
-		String actual = driver.getTitle();
-		if(expected.equals(actual)) {
-			System.out.println("passed");
-		}else {
-			System.out.println("failed");
-		}
-		System.out.println("expected: \t" + expected);
-		System.out.println("actual: \t" + actual);
+		driver.get("https://google.com");
 		
-		
+
 	}
-	
 
 }
